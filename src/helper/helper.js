526f64e7d8d89f0a47112c6e802e043585e8d556
unknown <aa@d.com>
2022-07-25 14:02:26 +0530

    done

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
 import { TextField } from '@material-ui/core'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+
 
 import React from 'react'
 
-export function CustomText({ name, value, handleChange, placeholder, error, required, label }) {
+export function CustomText({ name, value, handleChange, placeholder, error = {}, required, label }) {
   return (
     <>
       <h6 className="color-gray">
@@ -15,10 +18,10 @@ export function CustomText({ name, value, handleChange, placeholder, error, requ
         required={required}
         id={name}
         name={name}
-        value={value}
         variant="outlined"
-        defaultValue=""
-        error={false}
+        {...(value == '' ? {} : { value: value })}
+        error={error[name] ? true : false}
+        helperText={error[name] ? error[name] : false}
         type="text"
         placeholder={placeholder}
         fullWidth={true}
@@ -27,7 +30,37 @@ export function CustomText({ name, value, handleChange, placeholder, error, requ
     </>
   )
 }
-export function CustomEmail({ handleChange, name, placeholder, label, required }) {
+
+export function CustomFileUpload({ handleChange,error, value, name, label, required }) {
+  const getState = useSelector((state) => state)
+  const dispatch = useDispatch()
+  const {
+    media: {mediaOpen,fileFields,fileInput},
+  } = getState
+  useEffect(() => {
+    // if(fileFields[name]==undefined){
+       console.log({[name]: {id:value,url:""}})
+       dispatch({ type: "UpdateFileField", payload: {[name]: {id:value,url:"https://upload.wikimedia.org/wikipedia/commons/4/41/Sunflower_from_Silesia2.jpg"} } })
+     //}
+    }, [])
+    
+    function handleButton(){
+      console.log(fileFields)
+      dispatch({ type: "MediaOpen", payload: true })
+      dispatch({ type: "setFileInput", payload: name })
+    }
+    return (
+        <>
+         <h6 className="color-gray">
+        {label}
+        <sup>{required === true ? '*' : ''}</sup>
+      </h6>
+      <img width="100" src={fileFields?.[name]?.url} /><br/>
+      <button  onClick={(e)=> handleButton()} >Upload Image</button>
+    </>
+  )
+}
+export function CustomEmail({ handleChange, value, name, placeholder, label, required }) {
   return (
     <>
       <h6 className="color-gray">
@@ -41,7 +74,7 @@ export function CustomEmail({ handleChange, name, placeholder, label, required }
         name={name}
         type="email"
         variant="outlined"
-        defaultValue=""
+        {...(value == '' ? {} : { value: value })}
         error={false}
         placeholder={placeholder}
         fullWidth={true}
@@ -50,7 +83,7 @@ export function CustomEmail({ handleChange, name, placeholder, label, required }
     </>
   )
 }
-export function CustomPasssword({ handleChange, name, placeholder, label, required }) {
+export function CustomPasssword({ handleChange, value, name, placeholder, label, required }) {
   return (
     <>
       <h6 className="color-gray">
@@ -64,7 +97,7 @@ export function CustomPasssword({ handleChange, name, placeholder, label, requir
         name={name}
         type="password"
         variant="outlined"
-        value=""
+        {...(value == '' ? {} : { value: value })}
         error={false}
         placeholder={placeholder}
         fullWidth={true}
@@ -74,7 +107,7 @@ export function CustomPasssword({ handleChange, name, placeholder, label, requir
   )
 }
 
-export function CustomPhone({ handleChange, name, placeholder, label, required }) {
+export function CustomPhone({ handleChange, value, name, placeholder, label, required }) {
   return (
     <>
       <h6 className="color-gray">
@@ -88,7 +121,7 @@ export function CustomPhone({ handleChange, name, placeholder, label, required }
         name={name}
         type="number"
         variant="outlined"
-        value=""
+        {...(value == '' ? {} : { value: value })}
         error={false}
         placeholder={placeholder}
         fullWidth={true}
@@ -104,7 +137,6 @@ export function CustomSelect({ handleChange, options, value, name, label, requir
         {label}
         <sup>{required === true ? '*' : ''}</sup>
       </h6>
-      {console.log(options)}
       <select
         labelId="demo-simple-select-helper-label"
         id={name}
@@ -114,6 +146,9 @@ export function CustomSelect({ handleChange, options, value, name, label, requir
         fullWidth={true}
         onChange={(e) => handleChange(e)}
       >
+        <option key={'noselect'} value={''}>
+          Select..
+        </option>
         {options.map((ovalue, key) => {
           return (
             <option selected={value == ovalue.eid ? 'selected' : ''} key={ovalue.eid} value={ovalue.eid}>
@@ -122,6 +157,29 @@ export function CustomSelect({ handleChange, options, value, name, label, requir
           )
         })}
       </select>
+    </>
+  )
+}
+export function CustomTimeInput({ handleChange, value, name, placeholder, label, required }) {
+  return (
+    <>
+      <h6 className="color-gray">
+        {label}
+        <sup>{required === true ? '*' : ''}</sup>
+      </h6>
+      <TextField
+        className="custom-form-field"
+        required={required}
+        id={name}
+        name={name}
+        type="time"
+        variant="outlined"
+        {...(value == '' ? {} : { value: value })}
+        error={false}
+        placeholder={placeholder}
+        fullWidth={true}
+        onChange={handleChange}
+      />
     </>
   )
 }
